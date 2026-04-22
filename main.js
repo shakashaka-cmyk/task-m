68c49a0bb5c40de37e527b15db07d9f221c25ac6
しゃかしゃか <shakashaka1229@gmail.com>
2026-04-22 14:24:53 +0900

    1.課題の追加・保存・表示機能を作りました

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const addTaskForm = document.getElementById("add-task-form");
 const addButton = document.getElementById("add-button");
 const displaying = document.getElementById("displaying");
+const tasklist = document.getElementById("task-list");
 if (addButton && addTaskForm && displaying) {
     addButton.addEventListener('click', () => {
         displaying.style.display = "none";
@@ -21,5 +22,23 @@ addTaskForm === null || addTaskForm === void 0 ? void 0 : addTaskForm.addEventLi
         completed: false
     };
     console.log(task);
+    console.log("①");
+    changeDisplay();
+    console.log("②");
+    renderTask(task);
+    console.log("③");
 });
+const renderTask = (task) => {
+    const li = document.createElement("li");
+    li.textContent = `${task.title} | ${task.deadline} | ${task.importance}`;
+    tasklist === null || tasklist === void 0 ? void 0 : tasklist.appendChild(li);
+    console.log("task rendered");
+};
+function changeDisplay() {
+    if (displaying && addTaskForm) {
+        displaying.style.display = "block";
+        addTaskForm.style.display = "none";
+        console.log("display changed");
+    }
+}
 export {};
--- main.ts
+++ main.ts
@@ -3,6 +3,7 @@ import type { Task } from "./task";
 const addTaskForm = document.getElementById("add-task-form");
 const addButton = document.getElementById("add-button");
 const displaying = document.getElementById("displaying");
+const tasklist = document.getElementById("task-list");  
 
 if (addButton && addTaskForm && displaying) { 
     addButton.addEventListener('click', () => {
@@ -10,6 +11,7 @@ if (addButton && addTaskForm && displaying) {
         addTaskForm.style.display = "block";
     })
 };
+
 
 addTaskForm?.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -26,7 +28,26 @@ addTaskForm?.addEventListener('submit', (e) => {
     deadline,
     importance: importance as 1 | 2 | 3,
     completed: false
-};
-
+    }
     console.log(task);
+    console.log("①");
+    changeDisplay();
+    console.log("②");
+    renderTask(task);
+    console.log("③");
 });
+
+const renderTask = (task: Task) => {
+    const li = document.createElement("li");
+    li.textContent = `${task.title} | ${task.deadline} | ${task.importance}`;
+    tasklist?.appendChild(li);
+    console.log("task rendered");
+}
+
+function changeDisplay() {
+    if (displaying && addTaskForm) {
+        displaying.style.display = "block";
+        addTaskForm.style.display = "none";
+        console.log("display changed");
+    }
+}
